6089ec45246da27b03a6ad39e4833912e08df3ea
aizhuzhudegua <1648476050@qq.com>
2025-08-21 16:54:08 +0800

    detanormal loss

--- train.py
+++ train.py
@@ -195,6 +195,10 @@ def training(dataset, opt, pipe, dataset_name, testing_iterations, saving_iterat
         if iteration > 5000:
             losses_extra['predicted_normal'] = predicted_normal_loss(render_pkg["normal"], render_pkg["normal_ref"], render_pkg["alpha"])
             writer.add_scalar('Loss/predicted_normal', losses_extra['predicted_normal'], iteration)
+            if "delta_normal_norm" in render_pkg.keys():
+                losses_extra['delta_reg'] = delta_normal_loss(render_pkg["delta_normal_norm"], render_pkg["alpha"])
+                writer.add_scalar('Loss/Delta_Reg', losses_extra['delta_reg'].item(), iteration)
+                losses_extra['zero_one'] = zero_one_loss(render_pkg["alpha"])
 
 
         gt_image = viewpoint_cam.original_image.cuda()
